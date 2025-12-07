5072dbc64bda2538141458fb180d36eb1209bde3
Ian Chu <ian.chu@shopline.com>
2025-12-07 23:45:10 +0800

    remove performance now

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,27 +1,15 @@
-/* eslint-disable react-hooks/purity */
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import List from "./List";
 
 function UserList({ users }) {
-  const renderStart = useRef(performance.now());
-  renderStart.current = performance.now();
-
   const [count, setCount] = useState(0);
   const [query, setQuery] = useState("");
 
   const filteredUsers = users.filter((user) => user.name.includes(query));
 
-  // 2. 使用 useCallback 優化事件處理
   const handleClick = (id) => {
     console.log("click", id);
   };
-
-  useEffect(() => {
-    const renderEnd = performance.now();
-    console.log(
-      `After - took: ${(renderEnd - renderStart.current).toFixed(2)} ms`
-    );
-  });
 
   return (
     <div>
