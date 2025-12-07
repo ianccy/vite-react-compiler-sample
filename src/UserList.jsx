99dc4afc427aafa4fe6c640d8c7c94d583558a43
Ian Chu <ian.chu@shopline.com>
2025-12-07 23:49:48 +0800

    update timer

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import List from "./List";
 
 function UserList({ users }) {
@@ -7,13 +7,28 @@ function UserList({ users }) {
 
   const filteredUsers = users.filter((user) => user.name.includes(query));
 
+  // 2. 使用 useCallback 優化事件處理
   const handleClick = (id) => {
     console.log("click", id);
   };
 
+  useEffect(() => {
+    window.renderEnd = performance.now();
+    console.log(
+      `After - took: ${(window.renderEnd - window.renderStart).toFixed(2)} ms`
+    );
+  });
+
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Increment Count</button>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          window.renderStart = performance.now();
+        }}
+      >
+        Increment Count
+      </button>
       Count is {count}.<div>----</div>
       <input
         type="text"
